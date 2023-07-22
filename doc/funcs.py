9d58efa3d04a1c8f65ca3017ad0a49b294a721fe
Victor Stinner <vstinner@python.org>
2023-07-22 23:45:38 +0200

    Update stats

--- doc/funcs.py
+++ doc/funcs.py
@@ -3,9 +3,9 @@ import os
 import subprocess
 from pythoncapi import get_functions
 
-os.chdir('/home/vstinner/python/3.12')
+#os.chdir('/home/vstinner/python/3.12')
 #os.chdir('/home/vstinner/python/2.7')
-#os.chdir('/home/vstinner/python/main')
+os.chdir('/home/vstinner/python/main')
 public, private, internal = get_functions()
 for name in sorted(private):
     print(name)
